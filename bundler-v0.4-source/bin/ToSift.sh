8c30f2fc6611ef90115296003fc55dea2b12963b
Kevin Chiu <kevin@kevinchiu.org>
2011-01-12 22:39:07 -0500

    update

--- bundler-v0.4-source/bin/ToSift.sh
+++ bundler-v0.4-source/bin/ToSift.sh
@@ -30,8 +30,11 @@ else
 fi
 
 for d in `ls -1 $IMAGE_DIR | egrep "jpg$"`
-do 
+  do 
     pgm_file=$IMAGE_DIR/`echo $d | sed 's/jpg$/pgm/'`
     key_file=$IMAGE_DIR/`echo $d | sed 's/jpg$/key/'`
     echo "mogrify -format pgm $IMAGE_DIR/$d; $SIFT < $pgm_file > $key_file; rm $pgm_file; gzip -f $key_file"
-done
+    #echo "wc -l $key_file.vlfeat | awk '{ print \$1 \" 128\" }' > $key_file"
+    #echo "awk 'BEGIN { split(\"4 24 44 64 84 104 124 132\", offsets); } { i1 = 0; tmp = \$1; \$1 = \$2; \$2 = tmp; for (i=1; i<9; i++) { 12 = offsets[i]; out = \"\"; for (j=i1+1; j<=i2; j++) {  if (j != i1+1) { out = out \" \" echo "rm $key_file.vlfeat"
+    #echo "gzip -f $key_file"
+  done
